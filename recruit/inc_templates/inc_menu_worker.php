b3f82b196fc57e9ae21ec2f38b33360ebf80cf26
David RiddellNet <david+du@riddellnet.com>
2024-10-28 13:32:17 +0700

    change text

--- recruit/inc_templates/inc_menu_worker.php
+++ recruit/inc_templates/inc_menu_worker.php
@@ -10,7 +10,7 @@
     data-caption="職場密着動画　高山さん（営業）"  
     data-thumb="img/oneday/day_02.jpg?<?= VER ?>">
     <figure class="media-mov"><img src="img/oneday/day_02.jpg?<?= VER ?>" alt="職場密着動画　高山さん（営業）"></figure>
-    <h3 class="v-txt01"><small>高山さん</small><br>営業</h3>
+    <h3 class="v-txt01"><small>高山</small><br>営業</h3>
     </a>
 </li>
 <li>
@@ -19,7 +19,7 @@
     data-caption="職場密着動画　伊藤さん（営業）" 
     data-thumb="img/oneday/day_03.jpg?<?= VER ?>">
     <figure class="media-mov"><img src="img/oneday/day_03.jpg?<?= VER ?>" alt="職場密着動画　伊藤さん（営業）"></figure>
-        <h3 class="v-txt01"><small>伊藤さん</small><br>営業</h3>
+        <h3 class="v-txt01"><small>伊藤</small><br>営業</h3>
     </a>
 </li>
 <li>
@@ -28,7 +28,7 @@
     data-caption="職場密着動画　小峰さん（営業・営業企画）" 
     data-thumb="img/oneday/day_04.jpg?<?= VER ?>">
     <figure class="media-mov"><img src="img/oneday/day_04.jpg?<?= VER ?>" alt="職場密着動画　小峰さん（営業・営業企画）"></figure>
-        <h3 class="v-txt01 v-o-up"><small>小峰さん</small><br>営業・営業企画</h3>
+        <h3 class="v-txt01 v-o-up"><small>小峰</small><br>営業・営業企画</h3>
     </a>
 </li>
 <li>
@@ -37,7 +37,7 @@
     data-caption="職場密着動画　山路さん（AnewS）" 
     data-thumb="img/oneday/day_05.jpg?<?= VER ?>">
     <figure class="media-mov"><img src="img/oneday/day_05.jpg?<?= VER ?>" alt="職場密着動画　山路さん（AnewS）"></figure>
-        <h3 class="v-txt01 v-o-up"><small>山路さん</small><br>Anews</h3>
+        <h3 class="v-txt01 v-o-up"><small>山路</small><br>Anews</h3>
     </a>
 </li>
 </ul>
